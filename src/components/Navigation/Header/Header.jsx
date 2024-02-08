8690782e32544cd78f1549b89e59618c82463a21
PranavPatel-kalantagrata <pranavpatel.work@gmail.com>
2024-02-08 15:21:07 -0500

    hamburger updated

--- src/components/Navigation/Header/Header.jsx
+++ src/components/Navigation/Header/Header.jsx
@@ -1,25 +1,47 @@
 import React from "react";
-import {Link, NavLink} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 import HeaderCSS from './Header.module.css'
 
 function Header() {
+
+    const [isChecked, setIsChecked] = useState(false);
+    const handleCheckboxChange = (event) => {
+        setIsChecked(event.target.checked);
+    };
+    const resetCheckboxValue = ()=>{
+        setIsChecked(false)
+    }
+    
+    
+
+
     return (
         <header>
-            <nav className={HeaderCSS.headerContainner}>
+            
+            <input className={HeaderCSS.input} type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+        
+            <nav className={HeaderCSS.mainContainner}>
+                
+                <div className={HeaderCSS.hamburgerContainner}>
+                    
+                </div>
+                    
+                <div className={HeaderCSS.logoContainner}>
+                    <Link to="/"><img src="/Logos/k_white.png" alt="Logo"/>kalantagrata</Link>
+                </div>  
+        
+                <div className={HeaderCSS.navbarContainner} >
 
-                    <div className={HeaderCSS.headerLogoContainner}>
-                        <Link to="/"><img src="/Logos/k_white.png" alt="Logo"/>kalantagrata</Link>
-                    </div>  
+                    <ul>
+                        <li> <NavLink to="/" style={({isActive})=>{return {color: isActive? "#d5d5d5":"",};}} onClick={resetCheckboxValue}> Home </NavLink> </li>
+                        {/* <li> <NavLink to="/Services"> Services </NavLink> </li> */}
+                        <li> <NavLink to="/BrandArt" style={({isActive})=>{return {color: isActive? "#d5d5d5":"",};}} onClick={resetCheckboxValue} > Brand Art </NavLink> </li>
+                        <li> <NavLink to="/About" style={({isActive})=>{return {color: isActive? "#d5d5d5":"",};}} onClick={resetCheckboxValue}> About </NavLink> </li>
+                    </ul>
 
-                    <div className={HeaderCSS.headerNavbarContainner}>
-                        <ul>
-                            <li> <NavLink to="/" style={({isActive})=>{return {color: isActive? "#d5d5d5":"",};}}> Home </NavLink> </li>
-                            {/* <li> <NavLink to="/Services"> Services </NavLink> </li> */}
-                            <li> <NavLink to="/BrandArt" style={({isActive})=>{return {color: isActive? "#d5d5d5":"",};}}> Brand Art </NavLink> </li>
-                            <li> <NavLink to="/About" style={({isActive})=>{return {color: isActive? "#d5d5d5":"",};}}> About </NavLink> </li>
-                        </ul>
-                    </div>
-
+                </div>
+            
             </nav>
         </header>
     )
